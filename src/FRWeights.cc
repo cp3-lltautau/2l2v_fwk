42c16371ca5ca5cfb6ea678e1e415c141d5fdeb7
Marco Musich <marco.musich@cern.ch>
2017-07-03 09:55:46 +0200

    Adding class to store and retrieve Fake Rate weights

--- src/FRWeights.cc
+++ src/FRWeights.cc
@@ -31,29 +31,26 @@ bool FRWeights::init(const string& WeightsFileName)
 
     std::vector<string> catL  = {"Fake e", "Fake #mu", "Fake #tau_{had}"};
     std::vector<string> binL  = {"Inc.", "Barrel", "Endcap", "Inc. (m_{T}>30)", "Barrel (m_{T}>30)", "Endcap (m_{T}>30)"};
-
     std::vector<string> cat   = {"FR_El", "FR_Mu", "FR_Ta"};
     std::vector<string> bin   = {"","_B","_E", "_TMCut", "_TMCut_B", "_TMCut_E"};
     std::vector<string> var   = {"", "_Id_Iso01weight", "_Id_Iso02weight", "_Id_Iso03weight", "_Id_IsoLoweight", "_Id_IsoMeweight"};
     std::vector<string> wrt   = {"_wrtJetPt", "_wrtLepPt"};
-
+    
     for(unsigned int c=0;c<cat.size();c++){
       for(unsigned int b=0;b<bin.size();b++){
-	       for(unsigned int v=0;v<var.size();v++){
-	          for(unsigned int w=0;w<wrt.size();w++){
-               TString graphName = cat[c]+"FRWeights"+var[v]+bin[b]+wrt[w];
-              // FRWeightGraphs[cat[c]+var[v]+bin[b]+wrt[w]] = (TGraphErrors*)WeightsFile->Get((cat[c]+"FRWeights"+var[v]+bin[b]+wrt[w]).c_str());
-                std::cout<<" Yol - "<< graphName << std::endl;
-               FRWeightGraphs[cat[c]+var[v]+bin[b]+wrt[w]] = (TGraphErrors*)WeightsFile->Get(graphName);
-	            }
-	           }
-           }
-         }
-
+	for(unsigned int v=0;v<var.size();v++){
+	  for(unsigned int w=0;w<wrt.size();w++){
+	    FRWeightGraphs[cat[c]+var[v]+bin[b]+wrt[w]] = (TGraphErrors*)WeightsFile->Get((cat[c]+"FRWeights"+var[v]+bin[b]+wrt[w]).c_str());   
+	  }
+	}
+      }
+    }
+    
     WeightsFile->Close();
-
+    
     return true;
-  }
+  } 
+
 }
 
 /*****************************************************************/
@@ -68,6 +65,6 @@ double FRWeights::getWeight(const std::string& cat ,const std::string& bin, cons
   } else {
     result = 1;
   }
-  cout<<" Result = "<<result<<endl;
+
   return result;
 }
