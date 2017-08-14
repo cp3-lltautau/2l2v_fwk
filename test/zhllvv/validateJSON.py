91c0d048e647f954fdaaac109d57e493be84512e
Marco Musich <marco.musich@cern.ch>
2017-08-14 17:31:10 +0200

    improve script

--- test/zhllvv/validateJSON.py
+++ test/zhllvv/validateJSON.py
@@ -6,15 +6,25 @@ read_json_files = []
 
 def parse():
     for files in os.listdir(os.getcwd()):
-        print files
-        with open(files) as json_file:
-            try:
-                json.load(json_file)
-                read_json_files.append(files)
-            except ValueError, e:
-                print ("JSON object issue: %s") % e
-                invalid_json_files.append(files)
-    print invalid_json_files, len(read_json_files)
+        if ((os.path.isdir(files) == False) and files.endswith('.json'))  :
+            #print files
+            with open(files) as json_file:
+                try:
+                    json.load(json_file)
+                    read_json_files.append(files)
+                except ValueError, e:
+                    print ("file %s, JSON object issue: %s") % (files,e)
+                    invalid_json_files.append(files)
+
+    print "\n\n\n"
+    print "The following files are not valid JSON: \n"
+    listToDelete=""
+    for ex in invalid_json_files:
+        listToDelete+=" "
+        listToDelete+=ex
+
+    print listToDelete,"\n\n"
+    print "there are ",len(read_json_files),"valid JSON files"
 
 def main():
     # my code here
